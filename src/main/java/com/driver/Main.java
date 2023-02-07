ffd28aa6e0747dd753e08c3339176a434f3f383e
Taniya <tanya.kalra200@gmail.com>
2023-02-08 03:33:38 +0530

    First submission

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+        rwOnly.setName("Tanya");
+        System.out.println(rwOnly.getName());
+        //name has private access in com.driver.RWOnly
+    }
   
 }
